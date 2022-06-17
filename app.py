fe28f36d9bd501217d5f0c07d66bc806b392eba6
Trophy369 <98316317+Trophy369@users.noreply.github.com>
2022-06-17 05:11:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from webapp import db, migrate, create_app
 from webapp.auth.models import User, Referrer, Role, Transactions, Investments
 
 
-env = os.environ.get('WEBAPP_ENV', 'dev')
-app = create_app('config.%sConfig' % env.capitalize())
+#env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app()
 
 UPLOAD_FOLDER = 'webapp/static/img/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -12,7 +12,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, migrate=migrate, Referrer=Referrer, Role=Role,
+    return dict(db=db, User=User, migrate=migrate, Referrer=Referrer, Role=Role,
                 Transactions=Transactions, Investments=Investments)
 
 
