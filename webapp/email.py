a7ce03c6d2a38b6db212fa09321e4da42b2d7d37
Trophy369 <98316317+Trophy369@users.noreply.github.com>
2022-08-04 20:04:04 -0400

    Update email.py

--- webapp/email.py
+++ webapp/email.py
@@ -1,38 +1,40 @@
-from threading import Thread
+# from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
-from . import mail
+from . import mail, celery
 from flask_login import current_user
 
 
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+
+# def send_async_email(app, msg):
+#     with app.app_context():
+#         mail.send(msg)
 
 
-# def send_email(to, subject, template, **kwargs):
-#     app = current_app._get_current_object()
-#     msg = Message(app.config['CRYPTOWELL_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-#                   sender=app.config['CRYPTOWELL_MAIL_SENDER'], recipients=[to])
-#     msg.body = render_template(template + '.txt', **kwargs)
-#     msg.html = render_template(template + '.html', **kwargs)
-#     thr = Thread(target=send_async_email, args=[app, msg])
-#     thr.start()
-#     return thr
-
-
-def send_email(subject, sender, recipients, text_body, html_body):
+@celery.task
+def send_async_email(subject, sender, recipients, text_body, html_body):
+    """Background task to send an email with Flask-Mail."""
     app = current_app._get_current_object()
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
+    with app.app_context():
+        mail.send(msg)
+
+
+
+# def send_email(subject, sender, recipients, text_body, html_body):
+#     app = current_app._get_current_object()
+#     msg = Message(subject, sender=sender, recipients=recipients)
+#     msg.body = text_body
+#     msg.html = html_body
+#     Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Earncoins] Reset Your Password',
+    send_async_email('[Earncoins] Reset Your Password',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
@@ -43,7 +45,7 @@ def send_password_reset_email(user):
 
 def send_confirmation_email(user):
     token = user.generate_confirmation_token()
-    send_email('[Earncoins] Confirm your Email',
+    send_async_email('[Earncoins] Confirm your Email',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/confirm.txt',
@@ -53,41 +55,41 @@ def send_confirmation_email(user):
 
 
 def send_deposit_email(user):
-    send_email('[Earncoins] Deposit',
+    send_async_email('[Earncoins] Deposit',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               # text_body=render_template('email/deposit.txt',
-               #                           user=current_user.username),
+               text_body=render_template('email/deposit.txt',
+                                          user=current_user.username),
                html_body=render_template('email/deposit.html',
                                          user=current_user.username))
 
 
 def send_invest_email(user):
-    send_email('[Earncoins] Investment successful',
+    send_async_email('[Earncoins] Investment successful',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               # text_body=render_template('email/invest.txt',
-               #                           user=current_user.username),
+               text_body=render_template('email/invest.txt',
+                                          user=current_user.username),
                html_body=render_template('email/invest.html',
                                          user=current_user.username))
 
 
 def send_share_email(user):
-    send_email('[Earncoins] Share balance',
+    send_async_email('[Earncoins] Share balance',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               # text_body=render_template('email/share.txt',
-               #                           user=current_user.username),
+                text_body=render_template('email/share.txt',
+                                          user=current_user.username),
                html_body=render_template('email/share.html',
                                          user=current_user.username))
 
 
 def send_withdrawal_email(user):
-    send_email('[Earncoins] Withdrawal request',
+    send_async_email('[Earncoins] Withdrawal request',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               # text_body=render_template('email/withdrawal.txt',
-               #                           user=current_user.username),
+                text_body=render_template('email/withdrawal.txt',
+                                          user=current_user.username),
                html_body=render_template('email/withdrawal.html',
                                          user=current_user.username))
 
